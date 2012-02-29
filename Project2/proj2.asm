b61b386f2efe8703fcf56d9a4b462d9ec877140f
James Swaro <james.swaro@gmail.com>
2012-02-28 23:09:00 -0500

    Added project 2

---
+++ Project2/proj2.asm
@@ -0,0 +1,81 @@
+;           p1q1.asm
+;
+;           RUN:      Linux
+;           ASSEMBLE: nasm -l p1q1.lst -f elf p1q1.asm
+;           LINK:     ld -m elf_i386 -o p1q1 p1q1.o
+;
+;
+
+; Equivalent statements
+
+; Data segment
+SECTION .data
+
+; Un-initialized data segment
+SECTION .bss
+
+; Code segment
+SECTION .text
+GLOBAL filter
+
+filter:
+	push ebp               ; pushing stack base onto the stack
+	mov ebp, esp           ; moving stack pointer onto stack base
+
+	mov esi, [ebp + 8]     ; moving first argument into esi
+	mov edi, [ebp + 12]    ; moving second argument into edi
+
+	mov ecx, 0000005Dh     ; 93 decimal
+
+	mov ax, 0h             ; clearing ax
+	mov al, [esi]          ; move the contents of x[n-7] into al
+	cbw                    ; convert byte to word
+	mov bx, ax             ; move the word value of x[n-7] into bx
+
+	mov al, byte [esi + 1] ; move the contents of x[n-6] into al
+	cbw                    ; convert byte to word
+	add bx, ax             ; add x[n-7] and x[n-6], saving it into bx
+
+	mov al, byte [esi + 2] ; move the contents of x[n-5] into al
+	cbw                    ; convert byte to word
+	add bx, ax             ; add sum and x[n-5], saving it into bx
+
+	mov al, byte [esi + 3] ; move the contents of x[n-4] into al
+	cbw                    ; convert byte to word
+	add bx, ax             ; add sum and x[n-4], saving it into bx
+
+	mov al, byte [esi + 4] ; move the contents of x[n-3] into al
+	cbw                    ; convert byte to word
+	add bx, ax             ; add sum and x[n-3], saving it into bx
+
+	mov al, byte [esi + 5] ; move the contents of x[n-2] into al
+	cbw                    ; convert byte to word
+	add bx, ax             ; add sum and x[n-2], saving it into bx
+
+	mov al, byte [esi + 6] ; move the contents of x[n-1] into al
+	cbw                    ; convert byte to word
+	add bx, ax             ; add sum and x[n-1], saving it into bx
+
+.loop:
+	mov al, byte [esi + 7] ; move the contents of x[n] into al
+	cbw                    ; convert byte to word
+	add ax, bx             ; add sum and x[n], saving it into ax
+	mov dx, ax             ; save sum in dx
+
+	mov bl, 08h            ; move 8 into bl
+	idiv bl                ; divide the sum by 8
+	cbw                    ; byte extend the result
+	mov [edi + 14], ax     ; store result in y[n]
+
+	mov bx, dx             ; move sum into bx
+	mov al, byte [esi]     ; move x[n-7] into al
+	cbw                    ; convert byte to word
+	sub bx, ax             ; subtract the [n-7] from the sum
+
+	inc esi                ; move to the next element of x
+	add edi, 02h           ; move to the next element of y
+	loopnz .loop           ; while not finished, loop back
+
+	; Return
+	pop ebp                ; restore base pointer
+	ret                    ; return
---
+++ Project2/proj2.c
@@ -0,0 +1,79 @@
+/* ------------------------------------------------------------
+ 	
+	Project 2 - Template
+
+----------------------------------------------------------- */
+
+#include <stdio.h>
+
+
+int main( void )
+{
+	// Local variables
+  	int 	ii;
+	FILE	*in;
+	float	x[1024], y[1024], z[1024];
+	float	Hvalues[6] = {0.0};
+	float	Cvalues[3] = {0.0};	
+	float   csum[3]= {0, 0, 0};
+	float   hprod[6]= {0, 0, 0, 0, 0, 0};
+	
+	//
+	// Retrieve the variables from the input file
+	//
+	if ( (in = fopen("data.dat","r")) == NULL )
+	{
+		printf("Could not open file with x, y, and z vectors \n");
+		exit(0);
+	}
+
+	ii = 0;
+	while( (!feof(in)) && (ii < 1024) )
+	{
+		fscanf(in, "%f %f %f", &x[ii], &y[ii], &z[ii]);
+		ii++;
+	}
+
+	// Close the input file
+	fclose(in);
+
+	// ------------------------------------------------------
+	//	Insert your inline assembly code here to calculate
+	//	the Hvalues and Cvalues
+	// ------------------------------------------------------
+	_asm
+	{
+	}
+
+	// ------------------------------------------------------
+	//	To follow are the calculation of the values using C
+	// ------------------------------------------------------
+	for(ii=0;ii<1024;ii++) csum[0] = csum[0] + x[ii];
+	for(ii=0;ii<1024;ii++) csum[1] = csum[1] + y[ii];
+	for(ii=0;ii<1024;ii++) csum[2] = csum[2] + z[ii];
+	
+	//calculate h values
+	for(ii=0;ii<1024;ii++) hprod[0] = hprod[0] + x[ii]*x[ii];
+	for(ii=0;ii<1024;ii++) hprod[1] = hprod[1] + x[ii]*y[ii];
+	for(ii=0;ii<1024;ii++) hprod[2] = hprod[2] + x[ii]*z[ii];
+	for(ii=0;ii<1024;ii++) hprod[3] = hprod[3] + y[ii]*y[ii];
+	for(ii=0;ii<1024;ii++) hprod[4] = hprod[4] + y[ii]*z[ii];
+	for(ii=0;ii<1024;ii++) hprod[5] = hprod[5] + z[ii]*z[ii];
+	
+	
+	//print all results
+	printf("\nHvalues from C:\n");
+	for(ii=0;ii<6;ii++) printf("%f\n", hprod[ii]);
+	printf("\nHvalues from the Inline Assembly:\n");
+	for(ii=0;ii<6;ii++) printf("%f\n", Hvalues[ii]);
+	printf("\nCvalues from C:\n");
+	for(ii=0;ii<3;ii++) printf("%f\n", csum[ii]);	
+	printf("\nCvalues from the Inline Assembly:\n");
+	for(ii=0;ii<3;ii++) printf("%f\n", Cvalues[ii]);
+
+	getch();
+
+	return 0;
+}
+
+
