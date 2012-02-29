c694d9fa67480f57f8e8d42b04261234d354cc2a
James Swaro <james.swaro@gmail.com>
2012-02-29 14:53:04 -0500

    Finished the second section

--- Project2/proj2.asm
+++ Project2/proj2.asm
@@ -25,15 +25,15 @@ calculateHCValues:
 	push ebx                 ; store current value of ebx on stack
 	push ecx                 ; store current value of ecx on stack
 	push edx                 ; store current value of edx on stack
-        push edi                 ; store current value of edi on stack
-        push esi                 ; store current value of esi on stack
-        push eax                 ; store current value of eax on stack
+    push edi                 ; store current value of edi on stack
+    push esi                 ; store current value of esi on stack
+    push eax                 ; store current value of eax on stack
 
-	mov edx, [ebp + 8]	 ; store the first  argument into edx
+	mov edx, [ebp + 8]	     ; store the first  argument into edx
 	mov esi, [ebp + 12]      ; store the second argument into esi
 	mov edi, [ebp + 16]      ; store the third  argument into edi
-        mov eax, [ebp + 20]      ; store the fourth argument into eax
-        mov ebx, [ebp + 24]      ; store the fifth  argument into ebx
+    mov eax, [ebp + 20]      ; store the fourth argument into eax
+    mov ebx, [ebp + 24]      ; store the fifth  argument into ebx
 
 	mov [total], dword 00h
 	mov [total + 4], dword 00h
@@ -41,51 +41,52 @@ calculateHCValues:
 	mov [total + 12], dword 00h
 
 	mov ecx, 255             ; 
-        movups xmm0, [edx]       ; move x values
-        movups xmm1, [esi]       ; move y values
-        movaps xmm3, xmm0        ; sum of x_i
-        movaps xmm4, xmm1        ; sum of y_i
+    movups xmm0, [edx]       ; move x values
+    movups xmm1, [esi]       ; move y values
+    movaps xmm3, xmm0        ; sum of x_i
+    movaps xmm4, xmm1        ; sum of y_i
 
-        movaps xmm2, xmm1        ; mov y values into working register
-        mulps xmm2, xmm0         ; multiply x_i by y_i
-        movaps xmm5, xmm2        ; store result
+    movaps xmm2, xmm1        ; mov y values into working register
+    mulps xmm2, xmm0         ; multiply x_i by y_i
+    movaps xmm5, xmm2        ; store result
 
-        movaps xmm2, xmm0        ; mov x values into working register
-        mulps xmm2, xmm0         ; square x_i
-        movaps xmm6, xmm2        ; store result
+    movaps xmm2, xmm0        ; mov x values into working register
+    mulps xmm2, xmm0         ; square x_i
+    movaps xmm6, xmm2        ; store result
 
-        movaps xmm2, xmm1        ; mov y values into working regisrer
-        mulps xmm2, xmm1         ; square y_i
-        movaps xmm7, xmm2        ; store result
-        add edx, 10h
-        add esi, 10h
+    movaps xmm2, xmm1        ; mov y values into working regisrer
+    mulps xmm2, xmm1         ; square y_i
+    movaps xmm7, xmm2        ; store result
+    add edx, 10h
+    add esi, 10h
+
 .firstloop:
-        movups xmm0, [edx]       ; move x values
-        movups xmm1, [esi]       ; move y values
+    movups xmm0, [edx]       ; move x values
+    movups xmm1, [esi]       ; move y values
 
-        addps xmm3, xmm0         ; sum of x_i
+    addps xmm3, xmm0         ; sum of x_i
 
-        addps xmm4, xmm1         ; sum of y_i
+    addps xmm4, xmm1         ; sum of y_i
 
-        movaps xmm2, xmm1        ; mov y values into working register
-        mulps xmm2, xmm0         ; multiply x_i by y_i
-        addps xmm5, xmm2         ; store result
+    movaps xmm2, xmm1        ; mov y values into working register
+    mulps xmm2, xmm0         ; multiply x_i by y_i
+    addps xmm5, xmm2         ; store result
 
-        movaps xmm2, xmm0        ; mov x values into working register
-        mulps xmm2, xmm0         ; square x_i
-        addps xmm6, xmm2         ; store result
+    movaps xmm2, xmm0        ; mov x values into working register
+    mulps xmm2, xmm0         ; square x_i
+    addps xmm6, xmm2         ; store result
 
-        movaps xmm2, xmm1        ; mov y values into working regisrer
-        mulps xmm2, xmm1         ; square y_i
-        addps xmm7, xmm2         ; store result
-        add edx, 10h             ; move to next 4 values
-        add esi, 10h             ; move to next 4 values
-        loopnz .firstloop        ; continue while data left
+    movaps xmm2, xmm1        ; mov y values into working regisrer
+    mulps xmm2, xmm1         ; square y_i
+    addps xmm7, xmm2         ; store result
+    add edx, 10h             ; move to next 4 values
+    add esi, 10h             ; move to next 4 values
+    loopnz .firstloop        ; continue while data left
 
 	mov edx, [ebp + 8]       ; restore x initial position
 	mov esi, [ebp + 12]      ; restore y initial position
 
-        ;time to sum the sums of the data
+    ;time to sum the sums of the data
 	movups [total] , xmm3
 	call sum
 	mov eax, [total]
@@ -115,12 +116,87 @@ calculateHCValues:
 
 	mov ebx, [ebp + 24]
 
-	
+    ; start of second run through
 
+    mov ecx, 255             ; 255 decimal
+    movups xmm0, [edx]       ; move x values
+    movups xmm1, [esi]       ; move y values
+    movups xmm3, [edi]       ; move z values
+
+    movaps xmm4, xmm3        ; sum of z_i
+
+    movaps xmm2, xmm0        ; mov x values into working register
+    mulps xmm2, xmm3         ; multiply x_i by z_i
+    movaps xmm5, xmm2        ; store result
+
+    movaps xmm2, xmm3        ; mov z values into working register
+    mulps xmm2, xmm3         ; square z_i
+    movaps xmm6, xmm2        ; store result
+
+    movaps xmm2, xmm1        ; mov y values into working regisrer
+    mulps xmm2, xmm3         ; multiply y_i by z_i
+    movaps xmm7, xmm2        ; store result
+    add edx, 10h             ; move to next 4 values
+    add esi, 10h             ; move to next 4 values
+    add edi, 10h             ; move to next 4 values
+
+.secondloop:
+    movups xmm0, [edx]       ; move x values
+    movups xmm1, [esi]       ; move y values
+    movups xmm3, [edi]       ; move z values
+
+    addps xmm4, xmm3         ; sum of z_i
+
+    movaps xmm2, xmm0        ; mov y values into working register
+    mulps xmm2, xmm3         ; multiply x_i by z_i
+    addps xmm5, xmm2         ; sum result
+
+    movaps xmm2, xmm3        ; mov z values into working register
+    mulps xmm2, xmm3         ; square z_i
+    addps xmm6, xmm2         ; sum result
+
+    movaps xmm2, xmm1        ; mov y values into working regisrer
+    mulps xmm2, xmm3         ; multiply y_i by z_i
+    addps xmm7, xmm2         ; sum result
+
+    add edx, 10h             ; move to next 4 values
+    add esi, 10h             ; move to next 4 values
+    add edi, 10h             ; move to next 4 values
+    loopnz .secondloop       ; continue while data left
+
+	mov edx, [ebp + 8]       ; restore x initial position
+	mov esi, [ebp + 12]      ; restore y initial position
+    mov edi, [ebp + 16]      ; restore z initial position
+
+    ;time to sum the sums of the data
+	movups [total] , xmm4
+	call sum
+	mov eax, [total]
+	mov [ebx + 8], eax
+
+	mov eax, [ebp + 20]
+
+	movups [total], xmm5
+	call sum
+	mov ebx, [total]
+	mov [eax + 8], ebx
+
+	movups [total], xmm6
+	call sum
+	mov ebx, [total]
+	mov [eax + 20], ebx
+
+	movups [total], xmm7
+	call sum
+	mov dword ebx, [total]
+	mov dword [eax + 16], ebx
+
+	mov ebx, [ebp + 24]
+	
 	; Return
-        pop eax
-        pop esi
-        pop edi
+    pop eax
+    pop esi
+    pop edi
 	pop edx                  ; restore value of edx from stack
 	pop ecx                  ; restore value of ecx from stack
 	pop ebx                  ; restore value of ebx from stack
@@ -135,3 +211,4 @@ sum:
 	fadd dword [total + 12]
 	fstp  dword [total]
 	ret
+
